174c4f1a4da98072f0b24c99c54a8fa6b923301b
morrrrrrrr <moritz.weissenboeck@gmx.at>
2024-03-13 16:43:38 +0100

    Servo now has speed-control functionality

--- lib/servo/src/hardwareServo.cpp
+++ lib/servo/src/hardwareServo.cpp
@@ -1,57 +1,145 @@
 #include "hardwareServo.h"
 
 #include "common.h"
+#include <driver/ledc.h>
 
-#define NULLPIN 255
 
-Servo::Servo(uint8_t PWMChannel, uint8_t PWMResolution, uint8_t PWMTime) : 
-  m_PWMChannel(PWMChannel), m_PWMResolution(PWMResolution), m_PWMTime(PWMTime), m_attachedPin(NULLPIN)
+
+#define SERVO_FREQUENCY (1 / SERVO_PERIOD_TIME * 1000) 
+#define SERVO_DUTY_CYCLE_MAX (powf(2, SERVO_FREQUENCY) - 1)
+
+
+
+int mapftoi(float value, float fromLow, float fromHigh, int toLow, int toHigh)
 {
-  const int PWMFrequency = 1 / PWMTime * 1000;
-
-  ledcSetup(PWMChannel, PWMFrequency, PWMResolution);
-  m_dutyCycleMax = pow(2, PWMFrequency) - 1;
-
-  // calculate the low and high duty cycle points
-  m_dcLow = (m_thresholdLow  / m_PWMTime) * m_dutyCycleMax;
-  m_dcHigh = (m_thresholdLow  / m_PWMTime) * m_dutyCycleMax;
+    return ((value - fromLow) / fromHigh) * (toHigh - toLow) + toLow;
 }
+
+
+void Servo::isr_handler(void* servo_ptr)
+{
+    // Error-Handling: Check, if the argument is a nullptr
+    if (!servo_ptr) return;
+
+    // Re-interpret-cast the void* to a Servo* to access the servo
+    Servo* servo = reinterpret_cast<Servo*>(servo_ptr);
+
+    // set the moving flag of the servo to false
+    servo->m_isMoving = false;
+
+    return;
+}
+
+
+void Servo::setup()
+{
+    ledc_timer_config_t ledc_timer = {
+        .speed_mode =      SERVO_SPEED_MODE,
+        .duty_resolution = SERVO_DUTY_RESOLUTION,
+        .timer_num  =      SERVO_TIMER,
+        .freq_hz =         SERVO_FREQUENCY,
+        .clk_cfg =         LEDC_AUTO_CLK
+    };
+
+    ESP_ERROR_CHECK(ledc_timer_config(&ledc_timer));
+
+    ESP_ERROR_CHECK(ledc_fade_func_install(0));
+}
+
+Servo::Servo(ledc_channel_t ledc_channel, float low, float high) : 
+    m_ledc_channel(ledc_channel),
+    m_thresholdLow(low),
+    m_thresholdHigh(high),
+    m_dcLow ((m_thresholdLow   / SERVO_PERIOD_TIME) * SERVO_DUTY_CYCLE_MAX),
+    m_dcHigh((m_thresholdHigh  / SERVO_PERIOD_TIME) * SERVO_DUTY_CYCLE_MAX),
+    m_isAttached(false), m_isMoving(false),
+    m_attachedPin(0),
+    m_currentDutyCycle(0),
+    m_intr_handle()
+{ /* do nothing */}
 
 void Servo::attach(uint8_t pin)
 {
-  // safe-guard to not attach twice
-  if (isAttached()) return;
+    // safe-guard to not attach twice
+    if (isAttached()) return;
 
-  // save the pin you attach to
-  m_attachedPin = pin;
+    // save the pin you attach to
+    m_attachedPin = pin;
 
-  // call the ledc api to attach the pwm channel to the specified pin
-  ledcAttachPin(pin, m_PWMChannel);
+    // set the attached-flag to true
+    m_isAttached = true;
+
+    // call the ledc api to attach the pwm channel to the specified pin
+    // ledcAttachPin(pin, m_PWMChannel);
+
+    ledc_channel_config_t ledc_channel = {
+        .gpio_num =   m_attachedPin,
+        .speed_mode = SERVO_SPEED_MODE,
+        .channel =    m_ledc_channel,
+        .intr_type =  LEDC_INTR_FADE_END,
+        .timer_sel =  SERVO_TIMER,
+        .duty =       0,
+        .hpoint =     0,
+        .flags = {
+            .output_invert = 0
+        }
+    };
+
+    ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
+
+    ESP_ERROR_CHECK(ledc_isr_register(isr_handler, this, ESP_INTR_FLAG_LEVEL3, &m_intr_handle));
 }
 
-bool Servo::isAttached()
+bool Servo::isAttached() const
 {
-  // check, if the pin is still the default value or not
-  return m_attachedPin != NULLPIN;
+    // check, if the pin is still the default value or not
+    return m_isAttached;
 }
 
-int mapf(float value, float fromLow, float fromHigh, int toLow, int toHigh)
+void Servo::write(float angle, uint32_t time, bool block)
 {
-  return ((value - fromLow) / fromHigh) * (toHigh - toLow) + toLow;
+    /* NOTE: angle is given in radians */
+
+    // Error-Handling: if the servo is moving, while the angle is changed, the angle cannot be changed.
+    if (m_isMoving)
+    {
+        return;
+    }
+
+    // map the angle to the dutycycle
+    int dc = mapftoi(angle, 0.0f, M_PI, m_dcLow, m_dcHigh);
+
+    if (time)
+    {
+        // start a fade operation to the duty cycle using the time
+        startFadeOperation(time, block, block);
+    }
+    else
+    {
+        // write the duty cycle to the pwm channel
+        writeDutyCycle(dc);
+    }
 }
 
-void Servo::write(float angle)
+void Servo::writeDutyCycle(int targetDuty)
 {
-  /* NOTE: angle is given in radians */
+    m_currentDutyCycle = targetDuty;
 
-  // map the angle to the dutycycle
-  int dc = mapf(angle, 0.0f, M_PI, m_dcLow, m_dcHigh);
+    // ledcWrite(m_PWMChannel, targetDuty);
 
-  // write the duty cycle to the pwm channel
-  writeDutyCycle(dc);
+    ESP_ERROR_CHECK(ledc_set_duty(SERVO_SPEED_MODE, m_ledc_channel, targetDuty));
+    ESP_ERROR_CHECK(ledc_update_duty(SERVO_SPEED_MODE, m_ledc_channel));
 }
 
-void Servo::writeDutyCycle(int dutyCycle)
+void Servo::startFadeOperation(int targetDuty, uint32_t time, bool block)
 {
-  ledcWrite(m_PWMChannel, dutyCycle);
+    m_isMoving = true;
+
+    ledc_set_fade_time_and_start(
+        SERVO_SPEED_MODE,
+        m_ledc_channel,
+        targetDuty,
+        time,
+        block ? LEDC_FADE_WAIT_DONE : LEDC_FADE_NO_WAIT
+    );
 }
--- lib/servo/src/hardwareServo.h
+++ lib/servo/src/hardwareServo.h
@@ -4,56 +4,86 @@
 
 #include "common.h"
 
+#include <driver/ledc.h>
+
+#define SERVO_TIMER                 LEDC_TIMER_0
+#define SERVO_DUTY_RESOLUTION       LEDC_TIMER_10_BIT
+#define SERVO_PERIOD_TIME           20 // in ms
+#define SERVO_SPEED_MODE            LEDC_LOW_SPEED_MODE
+
 class Servo
 {
 public:
-  /*
-   * Constructor for the Servo-Object
-   * @param PWMChannel    which PWM channel of the LEDC-Api will this servo use
-   * @param PWMResolution what is the PWM Resolution (max 20) (def 10)
-   * @param PWMtime       the T of the PWM in ms -> (def 20ms)
-   */
-  Servo(uint8_t PWMChannel, uint8_t PWMResolution = 10, uint8_t PWMtime = 20);
+    /*
+    * Constructor for the Servo-Object
+    * @param ledc_channel which PWM channel of the LEDC-Api will this servo use
+    * @param low          T on of the PWM when the servo is left most (ms)
+    * @param high         T on of the PWM when the servo is right most (ms)
+    */
+    Servo(ledc_channel_t ledc_channel, float low = 1.5f, float high = 2.5f);
 
 public:
-  /*
-   * Attach the servo to a certain pin
-   * @param pin the pin the servo will attach to
-   */
-  void attach(uint8_t pin);
+    /*
+     * Setup the use of servos by setting up the Timer
+     */
+    static void setup();
 
-  /*
-   * Returns weather or not the servo is attached to a pin or not
-   */
-  bool isAttached();
+    /*
+    * Attach the servo to a certain pin
+    * @param pin the pin the servo will attach to
+    */
+    void attach(uint8_t pin);
 
-  /*
-   * Write an angle to the PWM
-   * @param angle in radians (0 - PI)
-   */
-  void write(float angle);
+    /*
+    * Returns weather or not the servo is attached to a pin or not
+    */
+    bool isAttached() const;
+
+    /*
+    * Write an angle to the PWM
+    * @param angle    in radians (0 - PI)
+    * @param time     in ms (0 is instant)
+    * @param blocking only for time != 0: wait for the servo to reach its endpoint?
+    */
+    void write(float angle, uint32_t speed = 0, bool blocking = false);
 
 private:
-  /*
-   * Write a duty cycle to the attached pin
-   * @param dutyCycle the duty cycle (0 to (2**PWMResolution - 1))
-   */
-  void writeDutyCycle(int dutyCycle);
+    /*
+    * Write a duty cycle to the attached pin
+    * @param dutyCycle the duty cycle (0 to (2**PWMResolution - 1))
+    */
+    void writeDutyCycle(int targetDuty);
+
+    /*
+     * Start a Fade operation (= slow movement)
+     * Fade starts from the current duty cycle
+     * @param dutyCycle to which duty cycle will the fade be
+     * @param speed     the speed of the fade
+     */
+    void startFadeOperation(int targetDuty, uint32_t speed, bool blocking);
 
 private:
-  uint8_t m_PWMChannel;
-  uint8_t m_PWMResolution;
-  uint8_t m_PWMTime;
+    /*
+     * This gets called in the ISR when a fade operation ends
+     */
+    static void isr_handler(void* servo_ptr);
 
-  uint8_t m_attachedPin;
+private:
+    ledc_channel_t m_ledc_channel;
 
-  // this is 2**PWMResolution - 1, calculated only once
-  int m_dutyCycleMax;
+    uint8_t m_attachedPin;
 
-  const float m_thresholdLow =  1.5f;
-  const float m_thresholdHigh = 2.5f;
+    float m_thresholdLow =  1.5f;
+    float m_thresholdHigh = 2.5f;
 
-  int m_dcLow, m_dcHigh;
+    int m_dcLow, m_dcHigh;
+
+    bool m_isMoving = false;
+    bool m_isAttached = false;
+
+    int m_currentDutyCycle;
+
+    ledc_isr_handle_t m_intr_handle;
 
 };
 
